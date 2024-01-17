33c068e0bce55db9148cce99730b01029bfd9eef
dssmith <daniels@infoslips.com>
2024-01-17 11:48:21 +0200

    add small test on ReadPreview

--- src/OpenFontSharp.Tests/OpenFontReader.cs
+++ src/OpenFontSharp.Tests/OpenFontReader.cs
@@ -10,6 +10,28 @@ namespace OpenFontSharp.Tests
     }
 
     [Test]
+    public void ReadPreview_TrueTypeFont_Roboto_Should_Return_PreviewFontInfo()
+    {
+      //init open font reader
+      var reader = new OpenFontReader();
+
+      //get roboto fonts
+      var ttfFonts = _getAllFilePaths("../../../Resources/Fonts/TTF/Roboto", "*.ttf");
+
+      var lstFontInfo = new List<PreviewFontInfo>();
+      foreach (var font in ttfFonts)
+      {
+        var stream = new MemoryStream(File.ReadAllBytes(font));
+        lstFontInfo.Add(reader.ReadPreview(stream));
+      }
+
+      Assert.That(lstFontInfo.Count, Is.EqualTo(12));
+      var robotoItalic = lstFontInfo.FirstOrDefault(x => x.Name == "Roboto" && x.SubFamilyName == "Italic");
+      Assert.That(robotoItalic, Has.Property("PostScriptName"));
+    }
+
+
+    [Test]
     public void Read_TrueTypeFont_Roboto_Should_Return_Typeface_Info()
     {
       //init open font reader
@@ -29,6 +51,29 @@ namespace OpenFontSharp.Tests
       var robotoItalic = lstFontInfo.FirstOrDefault(x => x.Name == "Roboto" && x.FontSubFamily == "Italic");
       Assert.That(robotoItalic, Has.Property("PostScriptName"));
     }
+
+    [Test]
+    public void Read_OpenTypeFont_GreatVibes_Should_Return_Typeface_Info()
+    {
+      //init open font reader
+      var reader = new OpenFontReader();
+
+      //get roboto fonts
+      var otfFonts = _getAllFilePaths("../../../Resources/Fonts/OTF/great-vibes", "*.otf");
+
+      var lstFontInfo = new List<Typeface>();
+      foreach (var font in otfFonts)
+      {
+        var stream = new MemoryStream(File.ReadAllBytes(font));
+        lstFontInfo.Add(reader.Read(stream));
+      }
+
+      Assert.That(lstFontInfo.Count, Is.EqualTo(1));
+      var greatvibes = lstFontInfo.FirstOrDefault();
+      
+      Assert.That(greatvibes, Has.Property("PostScriptName"));
+    }
+
 
 
     private List<string> _getAllFilePaths(string rootDirectory,string pattern = "*.*")
